093f9562232ec91495894100c3c91b7f5ccbe243
realstone513 <ting6247@nate.com>
2023-01-09 17:03:58 +0900

    add gun flash

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -12,6 +12,7 @@ public class Gun : MonoBehaviour
     public Transform gunBarrelEnd;
     public ParticleSystem gunParticles;
     public ParticleSystem hitParticles;
+    public Light gunFlash;
 
     private void Awake()
     {
@@ -20,6 +21,8 @@ public class Gun : MonoBehaviour
         bulletLineRenderer = GetComponent<LineRenderer>();
         bulletLineRenderer.positionCount = 2;
         bulletLineRenderer.enabled = false;
+
+        gunFlash.enabled = false;
     }
 
     void Start()
@@ -37,18 +40,16 @@ public class Gun : MonoBehaviour
 
         if (Input.GetMouseButton(0))
         {
-            StartCoroutine(ShotEffect(Vector3.forward * gunData.fireDistance));
+            Shot();
             timer = gunData.timeBetFire;
         }
     }
 
-
-    // 발사 이펙트와 소리를 재생하고 총알 궤적을 그린다
     private IEnumerator ShotEffect(Vector3 hitPosition)
     {
-        // 라인 렌더러를 활성화하여 총알 궤적을 그린다
         bulletLineRenderer.enabled = true;
         gunParticles.Play();
+        gunFlash.enabled = true;
         //if (hit)
         hitParticles.transform.position = hitPosition;
         hitParticles.Play();
@@ -57,10 +58,28 @@ public class Gun : MonoBehaviour
         bulletLineRenderer.SetPosition(0, gunBarrelEnd.position);
         bulletLineRenderer.SetPosition(1, hitPosition);
 
-        // 0.03초 동안 잠시 처리를 대기
         yield return new WaitForSeconds(0.03f);
-
-        // 라인 렌더러를 비활성화하여 총알 궤적을 지운다
         bulletLineRenderer.enabled = false;
+        gunFlash.enabled = false;
+    }
+
+    private void Shot()
+    {
+        Vector3 hitPos;
+        Ray ray = new(gunBarrelEnd.position, gunBarrelEnd.forward);
+        if (Physics.Raycast(ray, out RaycastHit hit, gunData.fireDistance))
+        {
+            hitPos = hit.point;
+            //var target = hit.collider.GetComponent<CapsuleCollider>();
+            //if (target != null)
+            //{
+            //    target.OnDamage(gunData.damage, hitPos, hit.normal);
+            //}
+        }
+        else
+        {
+            hitPos = gunBarrelEnd.position + gunBarrelEnd.forward * gunData.fireDistance;
+        }
+        StartCoroutine(ShotEffect(hitPos));
     }
 }
